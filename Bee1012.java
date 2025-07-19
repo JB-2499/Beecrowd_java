e792ea91775c0012dda725dd15ee6f1c007e57d0
JB-2499 <Anothermailb@gmail.com>
2025-07-19 16:53:47 -0300

    Beecrowd 1012 - Java

---
+++ Bee1012.java
@@ -0,0 +1,25 @@
+package beecrowd;
+import java.util.Scanner;
+
+public class Bee1012 {
+    public static void main(String[] args) {
+        Scanner pergunta = new Scanner(System.in);
+        double A = pergunta.nextDouble();
+        double B = pergunta.nextDouble();
+        double C = pergunta.nextDouble();
+        
+        double triangulo = A*C/2;
+        double circulo = C*C*3.14159;
+        double trapezio = ((A + B)*C)/2;
+        double quadrado = B*B;
+        double retangulo = A*B;
+
+        System.out.printf("TRIANGULO: %.3f\n", triangulo);
+        System.out.printf("CIRCULO: %.3f\n", circulo);
+        System.out.printf("TRAPEZIO: %.3f\n", trapezio);
+        System.out.printf("QUADRADO: %.3f\n", quadrado);
+        System.out.printf("RETANGULO: %.3f\n", retangulo);
+
+        pergunta.close();
+    }
+}
