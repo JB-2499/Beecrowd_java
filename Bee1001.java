1104e2c80134392b7167ca5db244a4dbfb6bcbfe
João Barreto <Anothermailb@gmail.com>
2025-07-19 18:15:20 -0300

    Update Bee1001.java

--- Bee1001.java
+++ Bee1001.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class Bee1001 {
+    public static void main(String[] args) {
+        Scanner pergunta = new Scanner(System.in);
+        int numero1 = pergunta.nextInt();
+        int numero2 = pergunta.nextInt();
+        int X = numero1 + numero2;
+        System.out.println("X = "+X);
+        pergunta.close();
+    }
+}
