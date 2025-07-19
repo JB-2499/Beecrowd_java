62a54d886d311f8dd7ce67c4f86a26d58d8673fd
JB-2499 <Anothermailb@gmail.com>
2025-07-19 16:51:06 -0300

    Beecrowd 1006 - Java

---
+++ Bee1006.java
@@ -0,0 +1,14 @@
+package beecrowd;
+import java.util.Scanner;
+
+public class Bee1006 {
+    public static void main(String[] args) {
+        Scanner pergunta = new Scanner(System.in);
+        double A = pergunta.nextDouble();
+        double B = pergunta.nextDouble();
+        double C = pergunta.nextDouble();
+        double media = (A*2 + B*3 + C*5)/10;
+        pergunta.close();
+        System.out.printf("MEDIA = %.1f\n", media);
+    }
+}
