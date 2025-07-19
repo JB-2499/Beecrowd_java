2be53a3bb5dad9521f447f86a3ab0ad2f6a0b740
JB-2499 <Anothermailb@gmail.com>
2025-07-19 16:53:23 -0300

    Beecrowd 1011 - Java

---
+++ Bee1011.java
@@ -0,0 +1,12 @@
+package beecrowd;
+import java.util.Scanner;
+
+public class Bee1011 {
+    public static void main(String[] args) {
+        Scanner pergunta = new Scanner(System.in);
+        int raio = pergunta.nextInt();
+        double volume = 3.14159*(4.0/3)*raio*raio*raio;
+        System.out.printf("VOLUME = %.3f\n", volume);
+        pergunta.close();
+    }
+}
