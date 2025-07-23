be11192fa65d33ca211fd3b3e79a471d23f63d60
João Barreto <Anothermailb@gmail.com>
2025-07-23 11:45:00 -0300

    Bee1010.java

--- Bee1010.java
+++ Bee1010.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Bee1010 {
+public class Main {
     public static void main(String[] args) {
         Scanner pergunta = new Scanner(System.in);
         String input1 = pergunta.nextLine();
